8f30f68fdc1c2955fb4a026075d0d5e289e1fc4f
Anvar-Avenger <anvarindastries@gmail.com>
2021-05-08 16:42:18 +0500

    Files added

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
+});
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/admin', function () {
+    if (auth()->user() == null)
+        return redirect()->route('login');
+    return redirect()->route('login');
 });
